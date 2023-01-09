f606d98da2d9bb1b4322afb28279c0231add0e01
Suzanne Banasihan <sjb04@ymail.com>
2023-01-08 22:02:24 -0800

    intern test done

---
+++ _tests_/Intern.js
@@ -0,0 +1,32 @@
+const Intern = require('../lib/Intern.js');
+
+const testObject = {name: 'Nora', id: 3, email: "789@some.com", school: "school"}
+
+test('Checks for inherited Employee properties', () => {
+    const intern = new Intern(testObject);
+    expect(intern.name).toEqual(expect.any(String));
+    console.log(`Intern name: ${intern.name}`)
+    expect(intern.id).toEqual(expect.any(Number));
+    console.log(`Intern id: ${intern.id}`)
+    expect(intern.email).toEqual(expect.any(String));
+    console.log(`Intern email: ${intern.email}`)
+});
+
+test('Checks for school', () => {
+    const intern = new Intern(testObject);
+    expect(intern.school).toEqual(expect.any(String));
+    console.log(`Intern school: ${intern.school}`)
+});
+
+test('Gets Interns school', () => {
+    const intern = new Intern(testObject);
+    expect(intern.getSchool()).toEqual(expect.any(String));
+    console.log(`Intern getSchool(): ${intern.getSchool()}`)
+});
+
+test('Gets Intern role', () => {
+    const intern = new Intern(testObject);
+    expect(intern.getRole()).toBe('Intern');
+    console.log(`Intern getRole(): ${intern.getRole()}`)
+   
+});
