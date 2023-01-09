4d0a60de6d6b9d7320de3330af7d52d543f9497a
Suzanne Banasihan <sjb04@ymail.com>
2023-01-08 21:24:58 -0800

    employee test done

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,36 @@
+const Employee = require("../lib/Employee");
+
+test('Checks for required Employee properties', () => {
+    const employee = new Employee('anonymous', 1,'email@ymail.com');
+    expect(employee.name).toEqual(expect.any(String));
+    console.log(`Employee Name: ${employee.name}`)
+    expect(employee.id).toEqual(expect.any(Number));
+    console.log(`Employee ID: ${employee.id}`)
+    expect(employee.email).toEqual(expect.any(String));
+    console.log(`Employee Email: ${employee.email}`)
+});
+
+test('Gets employees name', () => {
+    const employee = new Employee('anonymous', 1,'email@ymail.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+    console.log(`Employee getName(): ${employee.getName()}`)
+});
+
+test('Gets employees id', () => {
+    const employee = new Employee('anonymous', 1,'email@ymail.com');
+    expect(employee.getId()).toEqual(expect.any(String));
+    console.log(`Employee getId(): ${employee.getId()}`)
+}); 
+
+test('Gets employees email', () => {
+    const employee = new Employee('anonymous', 1,'email@ymail.com');
+    expect(employee.getEmail()).toEqual(expect.any(String));
+    console.log(`Employee getEmail(): ${employee.getEmail()}`)
+});
+
+test('Gets employees role', () => {
+    const employee = new Employee('anonymous', 1,'email@ymail.com');
+    expect(employee.getRole()).toBe('Employee');
+    
+    console.log(`Employee getRole(): ${employee.getRole()}`)
+});
